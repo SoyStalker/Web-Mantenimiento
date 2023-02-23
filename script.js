482fd78249829831bbd6dcfce82b9525d46cc356
SoyStalker <soystalker10@gmail.com>
2023-02-23 03:10:14 -0300

    toda la web

---
+++ script.js
@@ -0,0 +1,23 @@
+const botones = document.querySelector("#boton")
+
+const stalker = document.querySelector("#stalker")
+
+const cuadrado = document.querySelector("#clase")
+
+const icon = document.querySelector("#icon1")
+
+const icon1 = document.querySelector("#icon2")
+
+botones.addEventListener('click', () => {
+    document.body.classList.toggle('white')
+
+    botones.classList.toggle('active')
+
+    stalker.classList.toggle('white')
+
+    cuadrado.classList.toggle('white')
+
+    icon.classList.toggle('white')
+
+    icon1.classList.toggle('white')
+})
